9f41198fce5ddf4d2a3f5deddfba96d53b6eed40
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-14 16:17:10 +0300

    Create source news view function

--- app/views.py
+++ app/views.py
@@ -15,6 +15,15 @@ def index():
 def category_news(category_name):
 
     '''
-    View root page function that returns the index page and its data
+    View category news page function that returns the category-news page and its data for the category selected
     '''
     return render_template('category-news.html',category_name=category_name)    
+
+
+@app.route('/source/<source_id>')
+def source_news(source_id):
+
+    '''
+    View source news page function that returns the source-news page and its data for the source selected
+    '''
+    return render_template('source-news.html')        
