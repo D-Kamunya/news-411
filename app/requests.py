4119792a835e6623bd1c6785d2a088796b526802
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-14 19:56:22 +0300

    Create fn get top headline to get trending top world headlines news

--- app/requests.py
+++ app/requests.py
@@ -1,8 +1,11 @@
 from app import app
 import urllib.request,json
 from .models import source
+from .models import article
 
 Source = source.Source
+
+Article = article.Article
 
 # Getting news api key
 api_key = app.config['NEWS_API_KEY']
@@ -59,3 +62,50 @@ def process_sources(sources_list):
         sources_results.append(source_object)
 
     return sources_results
+
+
+def get_top_headlines():
+    '''
+    Function that gets the json response (i.e world top headlines news) to our url request
+    '''
+    get_headlines_url = top_headlines_url.format(api_key)
+
+    with urllib.request.urlopen(get_headlines_url) as url:
+        get_headlines_data = url.read()
+        get_headlines_response = json.loads(get_headlines_data)
+
+        headlines_results = None
+
+        if get_headlines_response['articles']:
+            headlines_results_list = get_headlines_response['articles']
+            headlines_results = process_articles(headlines_results_list)
+            
+    return headlines_results
+
+
+def process_articles(articles_list):
+    '''
+    Function  that processes the articles result and transform them to a list of Objects
+
+    Args:
+        articles_list: A list of dictionaries that contain articles details
+
+    Returns :
+        articles_results: A list of articles objects
+    '''
+    articles_results = []
+    for article in articles_list:
+      if article.get('urlToImage'):
+        author = article.get('author')
+        title = article.get('title')
+        description = article.get('description')
+        url = article.get('url')
+        article_img = article.get('urlToImage')
+        published_at = article.get('publishedAt')
+        content = article.get('content')
+
+        article_object = Article(author,title,description,url,article_img,published_at,content)
+        articles_results.append(article_object)
+
+    return articles_results    
+
