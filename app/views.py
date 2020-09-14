e1be038e928adcf6b6f1cc76814acf160d701744
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-14 16:09:49 +0300

    Create category news view function

--- app/views.py
+++ app/views.py
@@ -9,3 +9,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+@app.route('/category/<category_name>')
+def category_news(category_name):
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    return render_template('category-news.html',category_name=category_name)    
